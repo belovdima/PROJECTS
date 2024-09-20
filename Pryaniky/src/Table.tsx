8761c13804066c0b33f701e6e4260e24356c1dbc
Dima Belov <belovzaza@gmail.com>
2024-09-20 15:59:10 +0300

    added logout button

--- Pryaniky/src/Table.tsx
+++ Pryaniky/src/Table.tsx
@@ -1,8 +1,17 @@
+import { useNavigate } from "react-router-dom"; 
+
 export const Table = () => {
+
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate("/login", { replace: true });
+    }
 
     return(
         <div>
             Вы на странице с таблицами
+            <button className="logout" onClick={handleClick}>Обратно на главную</button>
         </div>
     )
 }
