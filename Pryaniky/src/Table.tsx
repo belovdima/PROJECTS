6e59c2062f9be23d477132cb9f559431aee38577
Dima Belov <belovzaza@gmail.com>
2024-09-23 21:32:39 +0300

    added prettier

--- Pryaniky/src/Table.tsx
+++ Pryaniky/src/Table.tsx
@@ -4,13 +4,13 @@ import { useNavigate } from "react-router-dom";
 // Интерфейс для структуры данных таблицы
 interface DocumentData {
     id: string;
-    companySigDate: string; // Дата в формате ISO строки
+    companySigDate: string;
     companySignatureName: string;
     documentName: string;
     documentStatus: string;
     documentType: string;
     employeeNumber: string;
-    employeeSigDate: string; // Дата в формате ISO строки
+    employeeSigDate: string;
     employeeSignatureName: string;
 }
 
@@ -19,28 +19,29 @@ export const Table = () => {
     const [data, setData] = useState<DocumentData[] | null>(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
-    const navigate = useNavigate();
     const [editingRow, setEditingRow] = useState<DocumentData | null>(null);
     const [hoveredRow, setHoveredRow] = useState<number | null>(null);
 
-    
+    const navigate = useNavigate();
 
-
-    const API_GET_URL = 'https://test.v5.pryaniky.com/ru/data/v3/testmethods/docs/userdocs/get';
-    const API_POST_URL = 'https://test.v5.pryaniky.com/ru/data/v3/testmethods/docs/userdocs/create';
-    const API_DELETE_URL = 'https://test.v5.pryaniky.com/ru/data/v3/testmethods/docs/userdocs/delete'; // URL для удаления
-    const API_EDIT_URL = 'https://test.v5.pryaniky.com/ru/data/v3/testmethods/docs/userdocs/set'; // URL для редактирования
-
+    const API_GET_URL =
+        "https://test.v5.pryaniky.com/ru/data/v3/testmethods/docs/userdocs/get";
+    const API_POST_URL =
+        "https://test.v5.pryaniky.com/ru/data/v3/testmethods/docs/userdocs/create";
+    const API_DELETE_URL =
+        "https://test.v5.pryaniky.com/ru/data/v3/testmethods/docs/userdocs/delete"; // URL для удаления
+    const API_EDIT_URL =
+        "https://test.v5.pryaniky.com/ru/data/v3/testmethods/docs/userdocs/set"; // URL для редактирования
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const response = await fetch(API_GET_URL, {
-                    method: 'GET',
+                    method: "GET",
                     headers: {
-                        'Content-type': 'application/json',
-                        'x-auth': localStorage.getItem("token") || ''
-                    }
+                        "Content-type": "application/json",
+                        "x-auth": localStorage.getItem("token") || "",
+                    },
                 });
 
                 if (!response.ok) {
@@ -48,7 +49,6 @@ export const Table = () => {
                 }
 
                 const result = await response.json();
-                console.log(result.data);
 
                 if (result.error_code !== 0) {
                     throw new Error(result.error_text);
@@ -67,15 +67,15 @@ export const Table = () => {
 
     // Состояние для новой записи
     const [newRow, setNewRow] = useState<DocumentData>({
-        id: '', // Идентификатор будет генерироваться сервером
-        companySigDate: '',
-        companySignatureName: '',
-        documentName: '',
-        documentStatus: '',
-        documentType: '',
-        employeeNumber: '',
-        employeeSigDate: '',
-        employeeSignatureName: ''
+        id: "", // Идентификатор будет генерироваться сервером
+        companySigDate: "",
+        companySignatureName: "",
+        documentName: "",
+        documentStatus: "",
+        documentType: "",
+        employeeNumber: "",
+        employeeSigDate: "",
+        employeeSignatureName: "",
     });
 
     // Обработчик изменений в полях формы
@@ -83,7 +83,7 @@ export const Table = () => {
         const { name, value } = event.target;
         setNewRow((prev) => ({
             ...prev,
-            [name]: value
+            [name]: value,
         }));
     };
 
@@ -91,16 +91,16 @@ export const Table = () => {
     const addTd = async () => {
         try {
             const response = await fetch(API_POST_URL, {
-                method: 'POST',
+                method: "POST",
                 headers: {
-                    'Content-Type': 'application/json',
-                    'x-auth': localStorage.getItem("token") || ''
+                    "Content-Type": "application/json",
+                    "x-auth": localStorage.getItem("token") || "",
                 },
-                body: JSON.stringify(newRow)
+                body: JSON.stringify(newRow),
             });
 
             if (!response.ok) {
-                throw new Error('Failed to add new row');
+                throw new Error("Failed to add new row");
             }
 
             const result = await response.json();
@@ -111,21 +111,22 @@ export const Table = () => {
             }
 
             // Обновление таблицы с новой записью
-            setData((prevData) => prevData ? [...prevData, result.data] : [result.data]);
+            setData((prevData) =>
+                prevData ? [...prevData, result.data] : [result.data]
+            );
 
             // Очистка полей формы
             setNewRow({
-                id: '',
-                companySigDate: '',
-                companySignatureName: '',
-                documentName: '',
-                documentStatus: '',
-                documentType: '',
-                employeeNumber: '',
-                employeeSigDate: '',
-                employeeSignatureName: ''
+                id: "",
+                companySigDate: "",
+                companySignatureName: "",
+                documentName: "",
+                documentStatus: "",
+                documentType: "",
+                employeeNumber: "",
+                employeeSigDate: "",
+                employeeSignatureName: "",
             });
-
         } catch (err) {
             setError((err as Error).message);
         }
@@ -136,86 +137,86 @@ export const Table = () => {
         setEditingRow(item);
         setNewRow(item); // Также заполняем форму данными из выбранной строки
     };
-    
-    
+
     const handleDelete = async (id: string) => {
         try {
-            const response = await fetch(`${API_DELETE_URL}/${id}`, { // Убрал фигурные скобки вокруг API_DELETE_URL
-                method: 'POST',
+            const response = await fetch(`${API_DELETE_URL}/${id}`, {
+                // Убрал фигурные скобки вокруг API_DELETE_URL
+                method: "POST",
                 headers: {
-                    'Content-Type': 'application/json',
-                    'x-auth': localStorage.getItem('token') || ''
-                }
+                    "Content-Type": "application/json",
+                    "x-auth": localStorage.getItem("token") || "",
+                },
             });
-    
+
             if (!response.ok) {
-                throw new Error('Ошибка при удалении строки');
+                throw new Error("Ошибка при удалении строки");
             }
-    
+
             const result = await response.json();
-    
+
             if (result.error_code !== 0) {
                 throw new Error(result.error_text);
             }
-    
+
             // Успешное удаление: обновить данные
-            setData((prevData) => prevData?.filter((row) => row.id !== id) || null);
-    
+            setData(
+                (prevData) => prevData?.filter((row) => row.id !== id) || null
+            );
         } catch (error) {
-            console.error('Ошибка удаления:', error);
+            console.error("Ошибка удаления:", error);
             setError((error as Error).message);
         }
     };
-    
-    
 
     const saveChanges = async () => {
         if (!editingRow) {
-            console.error('Не выбрана строка для редактирования');
+            console.error("Не выбрана строка для редактирования");
             return;
         }
-    
+
         try {
-            const response = await fetch(`${API_EDIT_URL}/${editingRow.id}`, { // Убедись, что editingRow.id не undefined
-                method: 'POST',
+            const response = await fetch(`${API_EDIT_URL}/${editingRow.id}`, {
+                // Убедись, что editingRow.id не undefined
+                method: "POST",
                 headers: {
-                    'Content-Type': 'application/json',
-                    'x-auth': localStorage.getItem("token") || ''
+                    "Content-Type": "application/json",
+                    "x-auth": localStorage.getItem("token") || "",
                 },
-                body: JSON.stringify(newRow)
+                body: JSON.stringify(newRow),
             });
-    
+
             if (!response.ok) {
-                throw new Error('Failed to update row');
+                throw new Error("Failed to update row");
             }
-    
+
             const result = await response.json();
             if (result.error_code === 0) {
-                setData(prevData =>
-                    prevData ? prevData.map(row => (row.id === editingRow.id ? result.data : row)) : []
+                setData((prevData) =>
+                    prevData
+                        ? prevData.map((row) =>
+                              row.id === editingRow.id ? result.data : row
+                          )
+                        : []
                 );
                 setEditingRow(null);
                 // Очищаем форму после успешного сохранения
                 setNewRow({
-                    id: '',
-                    companySigDate: '',
-                    companySignatureName: '',
-                    documentName: '',
-                    documentStatus: '',
-                    documentType: '',
-                    employeeNumber: '',
-                    employeeSigDate: '',
-                    employeeSignatureName: ''
+                    id: "",
+                    companySigDate: "",
+                    companySignatureName: "",
+                    documentName: "",
+                    documentStatus: "",
+                    documentType: "",
+                    employeeNumber: "",
+                    employeeSigDate: "",
+                    employeeSignatureName: "",
                 });
             }
         } catch (err) {
             console.error(err);
         }
     };
-    
-    
-    
-
 
     // Обработка загрузки и ошибок
     if (loading) return <div>Загрузка...</div>;
@@ -224,7 +225,14 @@ export const Table = () => {
     return (
         <div>
             <h1>Таблица с данными</h1>
-            <button className="logout" onClick={() => { localStorage.clear(); navigate("/login", { replace: true }); }}>Обратно на главную</button>
+            <button
+                className="logout"
+                onClick={() => {
+                    localStorage.clear();
+                    navigate("/login", { replace: true });
+                }}>
+                Обратно на главную
+            </button>
 
             {data && data.length > 0 ? (
                 <table className="table">
@@ -243,12 +251,25 @@ export const Table = () => {
                     </thead>
                     <tbody>
                         {data.map((item: DocumentData, index: number) => (
-                            <tr key={item.id} onMouseEnter={() => setHoveredRow(index)} onMouseLeave={() => setHoveredRow(null)}>
+                            <tr
+                                key={item.id}
+                                onMouseEnter={() => setHoveredRow(index)}
+                                onMouseLeave={() => setHoveredRow(null)}>
                                 <td>
                                     {hoveredRow === index ? (
                                         <div>
-                                            <button onClick={() => handleEdit(item)}>Edit</button>
-                                            <button onClick={() => handleDelete(item.id)}>Delete</button>
+                                            <button
+                                                onClick={() =>
+                                                    handleEdit(item)
+                                                }>
+                                                Edit
+                                            </button>
+                                            <button
+                                                onClick={() =>
+                                                    handleDelete(item.id)
+                                                }>
+                                                Delete
+                                            </button>
                                         </div>
                                     ) : (
                                         index + 1
@@ -265,9 +286,6 @@ export const Table = () => {
                             </tr>
                         ))}
                     </tbody>
-
-
-
                 </table>
             ) : (
                 <div>Нет данных для отображения</div>
@@ -276,18 +294,73 @@ export const Table = () => {
             <div className="add">
                 <h1>Добавить данные</h1>
                 <div className="add__input">
-                    <input className="company-signature-date" type="text" name="companySigDate" placeholder="Company Sig Date (ISO)" value={newRow.companySigDate} onChange={handleInputChange} />
-                    <input className="company-signature-name" type="text" name="companySignatureName" placeholder="Company Signature Name" value={newRow.companySignatureName} onChange={handleInputChange} />
-                    <input className="document-name" type="text" name="documentName" placeholder="Document Name" value={newRow.documentName} onChange={handleInputChange} />
-                    <input className="document-status" type="text" name="documentStatus" placeholder="Document Status" value={newRow.documentStatus} onChange={handleInputChange} />
-                    <input className="document-type" type="text" name="documentType" placeholder="Document Type" value={newRow.documentType} onChange={handleInputChange} />
-                    <input className="employee-number" type="text" name="employeeNumber" placeholder="Employee Number" value={newRow.employeeNumber} onChange={handleInputChange} />
-                    <input className="employee-signature-date" type="text" name="employeeSigDate" placeholder="Employee Sig Date (ISO)" value={newRow.employeeSigDate} onChange={handleInputChange} />
-                    <input className="employee-signature-name" type="text" name="employeeSignatureName" placeholder="Employee Signature Name" value={newRow.employeeSignatureName} onChange={handleInputChange} />
+                    <input
+                        className="company-signature-date"
+                        type="text"
+                        name="companySigDate"
+                        placeholder="Company Sig Date (ISO)"
+                        value={newRow.companySigDate}
+                        onChange={handleInputChange}
+                    />
+                    <input
+                        className="company-signature-name"
+                        type="text"
+                        name="companySignatureName"
+                        placeholder="Company Signature Name"
+                        value={newRow.companySignatureName}
+                        onChange={handleInputChange}
+                    />
+                    <input
+                        className="document-name"
+                        type="text"
+                        name="documentName"
+                        placeholder="Document Name"
+                        value={newRow.documentName}
+                        onChange={handleInputChange}
+                    />
+                    <input
+                        className="document-status"
+                        type="text"
+                        name="documentStatus"
+                        placeholder="Document Status"
+                        value={newRow.documentStatus}
+                        onChange={handleInputChange}
+                    />
+                    <input
+                        className="document-type"
+                        type="text"
+                        name="documentType"
+                        placeholder="Document Type"
+                        value={newRow.documentType}
+                        onChange={handleInputChange}
+                    />
+                    <input
+                        className="employee-number"
+                        type="text"
+                        name="employeeNumber"
+                        placeholder="Employee Number"
+                        value={newRow.employeeNumber}
+                        onChange={handleInputChange}
+                    />
+                    <input
+                        className="employee-signature-date"
+                        type="text"
+                        name="employeeSigDate"
+                        placeholder="Employee Sig Date (ISO)"
+                        value={newRow.employeeSigDate}
+                        onChange={handleInputChange}
+                    />
+                    <input
+                        className="employee-signature-name"
+                        type="text"
+                        name="employeeSignatureName"
+                        placeholder="Employee Signature Name"
+                        value={newRow.employeeSignatureName}
+                        onChange={handleInputChange}
+                    />
                 </div>
                 <button onClick={addTd}>Add</button>
                 <button onClick={saveChanges}>Сохранить изменения</button>
-
             </div>
         </div>
     );
