38ae90aebaf7aaf81a471c8023d51bca1a17d8d1
Dima Belov <belovzaza@gmail.com>
2024-10-02 17:34:46 +0300

    added some comments

--- Pryaniky/src/App.tsx
+++ Pryaniky/src/App.tsx
@@ -1,16 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Login } from './Login';
-import { Table } from './Table'; // Компонент с таблицей
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; //импорт react роутера
+import { Login } from "./Login"; //компонент с логином
+import { Table } from "./Table"; // Компонент с таблицей
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="/table" element={<Table />} />
-                <Route path="*" element={<Login/>} /> {/* Редирект на /login, если путь не найден */}
+                <Route path="/login" element={<Login />} /> //ссылка на Login
+                <Route path="/table" element={<Table />} /> //ссылка на Table
+                <Route path="*" element={<Login />} /> //стандартная ссылка
             </Routes>
-
         </Router>
     );
 }
