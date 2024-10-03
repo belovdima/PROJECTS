2d2e683b5b714a3c22e7e85a40de01193c4e4936
Dima Belov <machete75111@gmail.com>
2024-10-03 20:57:34 +0300

    started list component

--- To-Do-List/src/main.tsx
+++ To-Do-List/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import "./styles/css/styles.css";
+import store from "./redux/store";
+import App from "./components/App";
 
-createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-        <App />
-    </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>
 );
