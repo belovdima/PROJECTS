c74950324e8f5cd60de45038564208a3e80b43f4
Dima Belov <machete75111@gmail.com>
2024-10-07 14:08:24 +0300

    added header and footer

---
+++ TravelHelper/src/components/Footer.tsx
@@ -0,0 +1,15 @@
+export const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="footer__info">
+                <p>© 2024 Travel Helper. All rights reserved.</p>
+                <p>Contact us: support@travelhelper.com</p>
+            </div>
+            <div className="footer__socials">
+                <a href="#">LinkedIn</a>
+                <a href="#">GitHub</a>
+                <a href="#">Twitter</a>
+            </div>
+        </div>
+    );
+};
--- TravelHelper/src/main.tsx
+++ TravelHelper/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App.tsx";
+import "./styles/css/styles.css";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
