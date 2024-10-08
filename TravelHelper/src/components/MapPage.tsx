c1859486bd5238d347320880a14b6dca927ff009
Dima Belov <machete75111@gmail.com>
2024-10-08 18:01:34 +0300

    added MAP!!!

---
+++ TravelHelper/src/components/MapPage.tsx
@@ -0,0 +1,37 @@
+import { useRef, useEffect } from "react";
+import mapboxgl from "mapbox-gl";
+
+// Указываем тип для ссылки на карту
+export const MapPage = () => {
+    const mapRef = useRef<mapboxgl.Map | null>(null); // Используем тип Map или null
+    const mapContainerRef = useRef<HTMLDivElement | null>(null); // Указываем тип HTMLDivElement или null
+
+    useEffect(() => {
+        if (mapContainerRef.current) {
+            // Проверяем, что ссылка на контейнер карты существует
+            mapboxgl.accessToken =
+                "pk.eyJ1IjoiYmVsb3ZkaW1hIiwiYSI6ImNtMjBpbmhscDBqa3cyam9lempzNDRwbjIifQ.Mgf7Th-mTg07hZ_-qKYIUw";
+            mapRef.current = new mapboxgl.Map({
+                container: mapContainerRef.current, // Теперь container точно HTMLDivElement
+                center: [0, 0], // Указываем начальные координаты
+                zoom: 2, // Указываем начальный зум
+            });
+        }
+
+        return () => {
+            if (mapRef.current) {
+                mapRef.current.remove(); // Проверяем, что карта существует перед удалением
+            }
+        };
+    }, []);
+
+    return (
+        <>
+            <div
+                id="map-container"
+                ref={mapContainerRef}
+                style={{ width: "100%", height: "500px" }}
+            />
+        </>
+    );
+};
