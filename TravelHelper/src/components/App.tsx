e72557551ed292ad87bc2c03f86a46724f114bf4
Dima Belov <machete75111@gmail.com>
2024-10-08 12:24:23 +0300

    added HomePage

--- TravelHelper/src/components/App.tsx
+++ TravelHelper/src/components/App.tsx
@@ -1,10 +1,12 @@
 import { Header } from "./Header";
 import { Footer } from "./Footer";
+import { HomePage } from "./HomePage";
 
 function App() {
     return (
         <>
             <Header />
+            <HomePage />
             <Footer />
         </>
     );
