3fd8d7d6a788bd56ccf56add2f6df33d5c10ecaa
Dima Belov <machete75111@gmail.com>
2024-10-08 12:24:23 +0300

    added HomePage

---
+++ TravelHelper/src/components/HomePage.tsx
@@ -0,0 +1,17 @@
+export const HomePage = () => {
+    return (
+        <div className="home">
+            <div className="home__content">
+                <h1 className="home__title">
+                    Добро пожаловать в Планировщик Путешествий!
+                </h1>
+                <p className="home__description">
+                    Наш сайт поможет вам спланировать идеальное путешествие,
+                    выбрать страны, рассчитать бюджет, и получить информацию о
+                    визовых требованиях.
+                </p>
+                <button className="home__btn">Выбрать моё путешествие</button>
+            </div>
+        </div>
+    );
+};
