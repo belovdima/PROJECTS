2626605ec9aa5e5c1314dfd51c6eb582f8a62e62
Dima Belov <machete75111@gmail.com>
2024-10-08 18:01:34 +0300

    added MAP!!!

--- TravelHelper/src/components/App.tsx
+++ TravelHelper/src/components/App.tsx
@@ -1,12 +1,14 @@
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { HomePage } from "./HomePage";
+import { MapPage } from "./MapPage";
 
 function App() {
     return (
         <>
             <Header />
             <HomePage />
+            <MapPage />
             <Footer />
         </>
     );
