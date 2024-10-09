0cfca37ac0a1777180b05d86fe50ed514967e678
Dmitry Belov <machete75111@gmail.com>
2024-10-09 23:45:54 +0300

    made map wider

--- TravelHelper/src/components/Header.tsx
+++ TravelHelper/src/components/Header.tsx
@@ -1,7 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 export const Header = () => {
+    const navigate = useNavigate();
+
     return (
         <div className="header">
-            <div className="header__logo">Logo</div>
+            <div
+                className="header__logo"
+                onClick={() => navigate("/homepage", { replace: true })}>
+                Logo
+            </div>
             <a href="">Конвертер валют</a>
         </div>
     );
