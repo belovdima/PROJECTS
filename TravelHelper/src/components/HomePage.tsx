0f112430dbb692de6432ceb0b7dac7a7b0f50fb1
Dmitry Belov <machete75111@gmail.com>
2024-10-12 21:38:51 +0300

    Added .env to .gitignore

--- TravelHelper/src/components/HomePage.tsx
+++ TravelHelper/src/components/HomePage.tsx
@@ -6,6 +6,8 @@ export const HomePage = () => {
     const mapContainerRef = useRef<HTMLDivElement | null>(null);
     const mapRef = useRef<mapboxgl.Map | null>(null);
 
+    const mapboxToken = import.meta.env.VITE_MAPBOX_TOKEN;
+
     const handleClick = () => {
         setMenuOpen(!menuOpen); // Переключение состояния меню
     };
@@ -13,8 +15,7 @@ export const HomePage = () => {
     // Загрузка и настройка карты
     useEffect(() => {
         if (mapContainerRef.current) {
-            mapboxgl.accessToken =
-                "pk.eyJ1IjoiYmVsb3ZkaW1hIiwiYSI6ImNtMjBpbmhscDBqa3cyam9lempzNDRwbjIifQ.Mgf7Th-mTg07hZ_-qKYIUw";
+            mapboxgl.accessToken = mapboxToken;
             mapRef.current = new mapboxgl.Map({
                 container: mapContainerRef.current,
                 center: [0, 0],
