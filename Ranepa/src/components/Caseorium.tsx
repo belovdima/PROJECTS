3b1a35ac922b525a4ecf13bba3b27208e6612f8b
Dima Belov <machete75111@gmail.com>
2024-10-16 21:40:31 +0300

    added last component

--- Ranepa/src/components/Caseorium.tsx
+++ Ranepa/src/components/Caseorium.tsx
@@ -1,5 +1,59 @@
+import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation"; // Импорт навигации
+import "./../styles/swiper/caseorium.css";
+
+// Import required modules
+import { Pagination, Navigation, Autoplay } from "swiper/modules";
+
 export const Caseorium = () => {
     return (
-        <div className="c__caseorium"> Компонент "Кейсориум исполненного"</div>
+        <div className="project-showcase-outer">
+            <div className="c__project-showcase">
+                <Swiper
+                    slidesPerView={1} // Отображение только одного слайда
+                    spaceBetween={30}
+                    autoplay={{
+                        delay: 3000, // Время между переключениями слайдов в миллисекундах
+                        disableOnInteraction: false, // Продолжать автоматическое переключение после взаимодействия
+                    }}
+                    navigation={true} // Включаем навигацию стрелками
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[Pagination, Navigation, Autoplay]} // Добавьте модули
+                    className="mySwiper">
+                    <SwiperSlide>
+                        <div className="project-card">
+                            <h3>Успешный проект 1</h3>
+                            <p>Описание успешного проекта 1.</p>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="project-card">
+                            <h3>Успешный проект 2</h3>
+                            <p>Описание успешного проекта 2.</p>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="project-card">
+                            <h3>Успешный проект 3</h3>
+                            <p>Описание успешного проекта 3.</p>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="project-card">
+                            <h3>Успешный проект 4</h3>
+                            <p>Описание успешного проекта 4.</p>
+                        </div>
+                    </SwiperSlide>
+                </Swiper>
+            </div>
+        </div>
     );
 };
--- Ranepa/src/components/Expert.tsx
+++ Ranepa/src/components/Expert.tsx
@@ -6,88 +6,90 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation"; // Импорт навигации
-import "./../styles/swiper/swiper.css";
+import "./../styles/swiper/expert.css";
 
 // import required modules
 import { Pagination, Navigation, Autoplay } from "swiper/modules";
 
 export const Expert = () => {
     return (
-        <div className="c__expert">
-            <Swiper
-                slidesPerView={3}
-                spaceBetween={30}
-                autoplay={{
-                    delay: 3000, // Время между переключениями слайдов в миллисекундах
-                    disableOnInteraction: false, // Продолжать автоматическое переключение после взаимодействия
-                }}
-                navigation={true} // Включаем навигацию стрелками
-                pagination={{
-                    clickable: true,
-                }}
-                modules={[Pagination, Navigation, Autoplay]} // Добавьте модули
-                className="mySwiper">
-                <SwiperSlide>
-                    <div className="expert-card">
-                        <h3>Эксперт 1</h3>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="expert-card">
-                        <h3>Эксперт 2</h3>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="expert-card">
-                        <h3>Эксперт 3</h3>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="expert-card">
-                        <h3>Эксперт 4</h3>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="expert-card">
-                        <h3>Эксперт 5</h3>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="expert-card">
-                        <h3>Эксперт 6</h3>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="expert-card">
-                        <h3>Эксперт 7</h3>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="expert-card">
-                        <h3>Эксперт 8</h3>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="expert-card">
-                        <h3>Эксперт 9</h3>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="expert-card">
-                        <h3>Эксперт 10</h3>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="expert-card">
-                        <h3>Эксперт 11</h3>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="expert-card">
-                        <h3>Эксперт 12</h3>
-                    </div>
-                </SwiperSlide>
-            </Swiper>
+        <div className="expert-outer">
+            <div className="c__expert">
+                <Swiper
+                    slidesPerView={3}
+                    spaceBetween={30}
+                    autoplay={{
+                        delay: 3000, // Время между переключениями слайдов в миллисекундах
+                        disableOnInteraction: false, // Продолжать автоматическое переключение после взаимодействия
+                    }}
+                    navigation={true} // Включаем навигацию стрелками
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[Pagination, Navigation, Autoplay]} // Добавьте модули
+                    className="mySwiper">
+                    <SwiperSlide>
+                        <div className="expert-card">
+                            <h3>Эксперт 1</h3>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="expert-card">
+                            <h3>Эксперт 2</h3>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="expert-card">
+                            <h3>Эксперт 3</h3>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="expert-card">
+                            <h3>Эксперт 4</h3>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="expert-card">
+                            <h3>Эксперт 5</h3>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="expert-card">
+                            <h3>Эксперт 6</h3>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="expert-card">
+                            <h3>Эксперт 7</h3>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="expert-card">
+                            <h3>Эксперт 8</h3>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="expert-card">
+                            <h3>Эксперт 9</h3>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="expert-card">
+                            <h3>Эксперт 10</h3>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="expert-card">
+                            <h3>Эксперт 11</h3>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="expert-card">
+                            <h3>Эксперт 12</h3>
+                        </div>
+                    </SwiperSlide>
+                </Swiper>
+            </div>
         </div>
     );
 };
